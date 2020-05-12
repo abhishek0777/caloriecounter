6a79c779c8faa79db251280b3570a4b4d5112e72
Roshanbhagat2071 <65238082+Roshanbhagat2071@users.noreply.github.com>
2020-05-12 21:22:31 +0530

    Front end development
    Today I have done login page and its CSS and start working on home page which opens up after login along with its CSS.

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>calorie counter</title>
+        <meta charset="UTF-8">
+        <link rel= "stylesheet" type='text/css' href="counter.css">
+    </head>
+    <body>
+        <div class="loginbox">
+            <img src="avatar.jpg" class="avatar">
+            <h1>Login here</h1>
+            <form>
+                <p title="Type your username">Username</p>
+                <input type="text" name="" placeholder="Enter username">
+                <p title="Enter your password">Password</p>
+                <input type="password" name="" placeholder= "Enter password">
+                <input type="submit"  name="" value="Login">
+                <a href="#">Lost your password?</a> <br>
+                <a href="index.php">Don't have an account?</a>
+            </form>
+
+        </div>
+    </body>
+
+</html>
